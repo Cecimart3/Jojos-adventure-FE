1f9a8a8e80effbf3d5fa1f3ec0817783c0399bde
Cecilia Martinez <cecimart3@gmail.com>
2022-03-29 18:18:34 -0400

    updated api url

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:6969';
+const BASE_URL = "https://jojos-adventure-be.herokuapp.com/";
 
 const storyApi = axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 const storyCalls = {
-    startStory: () => (storyApi.get('/story')),
-    nextStory: (id) => (storyApi.get(`/story/${id}`))
-}
+  startStory: () => storyApi.get("/story"),
+  nextStory: (id) => storyApi.get(`/story/${id}`),
+};
 
 export default storyCalls;
